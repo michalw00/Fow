6fed974db0b516d54fc161dc1c371aafc6d643d0
SanchoCC <sanchocat1313@gmail.com>
2025-03-20 13:56:39 +0100

    added changing player's turn

--- src/match/match.cc
+++ src/match/match.cc
@@ -11,5 +11,20 @@ namespace fow {
         for (auto& player : players_) {
             player.InitRenderMap(*map_, basic_width, basic_height);
         }
+
+        current_player_index_ = 0;
+
+    }
+
+    void Match::EndTurn() {
+        NextPlayer();
+    }
+
+    void Match::NextPlayer() {
+        if (current_player_index_ >= players_.size() - 1) {
+            current_player_index_ = 0;
+        } else {
+            ++current_player_index_;
+        }
     }
 }
--- src/match/match.h
+++ src/match/match.h
@@ -11,13 +11,20 @@ namespace fow {
 		void InitMap(int rows, int columns);
 		void InitPlayers(float basic_width, float basic_height, int number = 2);
 
+		void EndTurn();
+
 		void UpdateRenderMap();
 
 		Map GetMap() const { return *map_; }
 		std::vector<Player> GetPlayers() const { return players_; }
+		const Player& GetCurrentPlayer() const { return players_.at(current_player_index_); }
 	private:
+		void NextPlayer();
+
 		std::unique_ptr<Map> map_;
 		std::vector<Player> players_;
+
+		int current_player_index_;
 	};
 
 }
