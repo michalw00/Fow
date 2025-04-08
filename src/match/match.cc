cb575a56388ba676ab25eebf7d14c390aff02148
SanchoCC <sanchocat1313@gmail.com>
2025-04-08 18:33:27 +0200

    placed two units

--- src/match/match.cc
+++ src/match/match.cc
@@ -7,12 +7,12 @@ namespace fow {
 
     void Match::InitPlayers(float basic_width, float basic_height, RCamera2D camera, int number) {
         players_.resize(number);
-
         for (auto& player : players_) {
             player.InitCamera(camera);
             player.InitRenderMap(*map_, basic_width, basic_height);
         }
-
+        players_[0].AddUnit(1, 1, UnitType::kAntiTank);
+        players_[1].AddUnit(2, 2, UnitType::kInfantry);
         current_player_index_ = 0;
 
     }
