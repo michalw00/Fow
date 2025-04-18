c9ef3289bfb7bf31833932b2d227ef17a9761dd1
SanchoCC <sanchocat1313@gmail.com>
2025-04-18 18:49:22 +0200

    added rmb action on button

--- src/drawable/button/button.cc
+++ src/drawable/button/button.cc
@@ -1,13 +1,18 @@
 #include "button.h"
 
 namespace fow {
-    Button::Button(RVector2 position, std::function<void()> action)
-        : Drawable(position), action_(action) {}
+    Button::Button(RVector2 position, std::function<void()> action_lmb, std::function<void()> action_rmb)
+        : Drawable(position), action_lmb_(action_lmb), action_rmb_(action_rmb)  {}
 
     void Button::CheckMouse(RVector2 mouse_position) {
         is_hovered_ = area_.CheckCollision(mouse_position);
-        if (is_hovered_ && IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
-            action_();
+        if (is_hovered_) {
+            if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+                action_lmb_();
+            }
+            if (IsMouseButtonPressed(MOUSE_BUTTON_RIGHT)) {
+                action_rmb_();
+            }
         }
     }   
 }
--- src/drawable/button/button.h
+++ src/drawable/button/button.h
@@ -11,15 +11,18 @@
 namespace fow {
     class Button : public Drawable  {
     public:
-        Button(RVector2 position, std::function<void()> action);
+        Button(RVector2 position, std::function<void()> action_lmb, std::function<void()> action_rmb);
         virtual ~Button() {}; 
         void CheckMouse(RVector2 mouse_position);
 
         RRectangle GetArea() const { return area_; }
+        void SetIsSelected(bool is_selected) { is_selected_ = is_selected; }
     protected:
+        bool is_selected_ = false;
         bool is_hovered_ = false;
         RRectangle area_;
-        std::function<void()> action_;    
+        std::function<void()> action_lmb_; 
+        std::function<void()> action_rmb_; 
     };
 
 }
