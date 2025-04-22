5a521517e37046ffda15f91bfe42bc16e30bf5f6
SanchoCC <sanchocat1313@gmail.com>
2025-04-22 23:59:10 +0300

    added ChangeText and improved spacing calculation

--- src/drawable/text.cc
+++ src/drawable/text.cc
@@ -1,11 +1,17 @@
 #include "text.h"
 
 namespace fow {
-    Text::Text(RVector2 position, const RText& rtext, bool centered) : Drawable(position), rtext_(rtext) {
-        if (centered) {
-            RVector2 offset = rtext.MeasureEx();
-            position_ -= offset / 2.0;
+    Text::Text(RVector2 position, const RText& rtext, bool centered) : Drawable(position), rtext_(rtext), centered_(centered) {
+        Configurate();
+    }
+
+    void Text::ChangeText(std::string&& text) {
+        if (centered_) {
+            RVector2 offset = rtext_.MeasureEx();
+            position_ += offset / 2.0;
         }
+        rtext_.SetText(text);
+        Configurate();
     }
 
     void Text::Draw() const {
@@ -16,5 +22,13 @@ namespace fow {
         rtext_.spacing *= scale.x;
         rtext_.fontSize *= scale.x;
         position_ *= scale;
+    }
+
+    void Text::Configurate() {
+        rtext_.SetSpacing(rtext_.GetFontSize()*0.1f);
+        if (centered_) {
+            RVector2 offset = rtext_.MeasureEx();
+            position_ -= offset / 2.0;
+        }
     }
 }
--- src/drawable/text.h
+++ src/drawable/text.h
@@ -9,10 +9,15 @@ namespace fow {
 	class Text : public Drawable {
 	public:
 		Text(RVector2 position, const RText& rtext, bool centered = true);
+		void ChangeText(std::string&& text);
 
 		void Draw() const override;
 		void Scale(RVector2 scale) override;
 	private:
+		void Configurate();
+
+		bool centered_;
+
 		RText rtext_;
 	};
 }
