793b11f77134560f227d98f773a4ad17f26f244d
SanchoCC <sanchocat1313@gmail.com>
2025-04-22 20:55:22 +0300

    added static drawables; united PlaceText and PlaceButton in PlaceDrawable in Screen

--- src/core/screen/screen.cc
+++ src/core/screen/screen.cc
@@ -9,40 +9,53 @@ namespace fow {
         buttons_static_.clear();
     }
 
-    void Screen::Draw() const {  
+    void Screen::Draw() const {        
         camera_->BeginMode();
         for (auto& drawable : drawables_) {
             drawable->Draw();
-        }    
+        }
         camera_->EndMode();
+        for (auto& drawable_static : drawables_static_) {
+            drawable_static->Draw();
+        }
     }
 
     void Screen::CheckButtons() {
-        RVector2 mouse_position = camera_->GetScreenToWorld(GetMousePosition());
+        RVector2 mouse_position = GetMousePosition();
+        for (auto& button_static : buttons_static_) {
+            button_static->CheckMouse(mouse_position);
+        }
+        RVector2 mouse_position_world = camera_->GetScreenToWorld(mouse_position);
         for (auto& button : buttons_) {
-            button->CheckMouse(mouse_position);
+            button->CheckMouse(mouse_position_world);
         }
     }
 
-    void Screen::ScalePositions(float window_width, float window_height) {
+    void Screen::ScalePositions(float window_width, float window_height, bool inverse) {
         RVector2 scale = { window_width / basic_width_, window_height / basic_height_ };
+        if (inverse) {
+            scale = RVector2(1.f, 1.f ) / scale;
+        }
         for (auto& drawable : drawables_) {
             drawable->Scale(scale);
         }
+        for (auto& drawable_static : drawables_static_) {
+            drawable_static->Scale(scale);
+        }
     }
 
-    void Screen::AddRText(std::string&& name, RText&& rtext) {
-        rtext.SetSpacing(10.f);
-        rtexts_.emplace(std::move(name), rtext);
-    }
-
-    void Screen::PlaceText(std::shared_ptr<Text> text) {
-        drawables_.push_back(text);
-    }
-
-    void Screen::PlaceButton(std::shared_ptr<Button> button) {
-        drawables_.push_back(button);
-        buttons_.push_back(button);
+    void Screen::PlaceDrawable(std::shared_ptr<Drawable> drawable, bool is_static) {
+        if (is_static) {
+            drawables_static_.push_back(drawable);
+            if (drawable->GetIsButton()) {
+                buttons_static_.push_back(std::static_pointer_cast<Button>(drawable));
+            }
+        } else {
+            drawables_.push_back(drawable);
+            if (drawable->GetIsButton()) {
+                buttons_.push_back(std::static_pointer_cast<Button>(drawable));
+            }
+        }
     }
 
 }
